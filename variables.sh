19d9b90f3d4b90c3c6524ba72a55487fff7baed5
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-03-04 12:30:13 +0100

    changed order

--- variables.sh
+++ variables.sh
@@ -19,11 +19,11 @@ export ASTER_LIBS=${ASTER_BASE}/public
 # External libs   #
 ###################
 
-export EXTLIBS=${ASTER_BASE}/public/
-export OPENBLAS_DIR=${EXTLIBS}/OpenBLAS
 export MPI_DIR=/cad/app/openmpi/1.10.5
 export SYSTEM_LIBS=/usr/lib64
 export SYSTEM_INCLUDE=/usr/include
+export EXTLIBS=${ASTER_BASE}/public/
+export OPENBLAS_DIR=${EXTLIBS}/OpenBLAS
 
 ###################
 # Install dirs    #
